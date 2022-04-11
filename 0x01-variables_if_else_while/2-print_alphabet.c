f038d9140a7c7f1c17bbb564d18e2bb5554edf51
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-11 07:50:25 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * Return: 0
+ */
+in main(void)
+{
+	int i;
+
+	for (i = 97; i <= 117; i++)
+		putchar(i);
+	for (i = 119; i <= 122; i++)
+		putchar(i);
+	return (o);
+}
