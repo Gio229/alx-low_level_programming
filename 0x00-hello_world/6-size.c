d05b7853161a29b201aafd1e8c9c6213270c47d8
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-11 02:42:14 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,10 @@
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of along long int: %d byte(s)", sizeof(long long int));
+	printf("Size of float: %d byte(s)", sizeof(float));
 	return (0);
 }
