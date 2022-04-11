87d57d7040d15b746cf872e38a4b5f802c42bbde
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-11 07:55:15 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i <= 117; i++)
+	for (i = 97; i <= 122; i++)
 		putchar(i);
-	for (i = 119; i <= 122; i++)
-		putchar(i);
+
+	putchar('\n');
+
 	return (0);
 }
