dce0438d265db6da9c11f3880b8d901e2765da9e
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-11 01:52:11 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * main -entry point
+ * return: 0
 int main(void)
 {
-	puts("\"Progamming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
