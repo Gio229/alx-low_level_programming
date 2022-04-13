da4f69cd8c0be0581c837220113797ad7ad68e77
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-13 09:36:25 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check if is lower
+ *
+ * Return: 1 if true 0 else
+ */
+void _islower(int c)
+{
+
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
