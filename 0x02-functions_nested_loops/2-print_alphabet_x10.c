62158d0acdf39b941d070ae9f9b7283ed407dbe2
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-13 09:20:48 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,19 @@
-#include "main"
+#include "main.h"
 
-void print_alphabet(void)
+/**
+ * print_alphabet_x10 - print the alphabet x10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
+	int i, j;
+	for (i = 0; i <10; i++)
 	{
-		_putchar(i);
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
