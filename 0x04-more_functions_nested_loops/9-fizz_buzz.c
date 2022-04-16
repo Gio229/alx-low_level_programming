29423904f19455fed872e25cf6a4486198c5ee96
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-16 03:34:26 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 !=  0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			_putchar(' ');
+		else
+			_putchar('\n');
+	}
+	return (0);
+}
