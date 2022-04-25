c6ef2ff0941afa680eca3c0f8693c0e69b3b3c85
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-25 14:33:52 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ *
+ * @a: array
+ * @n:  number of elements of the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, start, end, middleSize, tmp;
+
+	for (i = 0; a[i] != '\0'; i++)
+		n++;
+
+	middleSize = (n / 2);
+	start = 0;
+	end = (n > 0) ? (n - 1) : 0;
+	for (i = 0; i < middleSize; i++)
+	{
+		if (start == end)
+			break;
+		tmp = a[start];
+		a[start] = a[end];
+		a[end] = tmp;
+
+		start++;
+		end--;
+	}
+}
