5148c518529fd06c05bfd5c7a59f54e58087751e
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-28 13:02:11 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,10 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	++s;
-	if (*s != '\0')
-		_puts_recursion(s);
-	_putchar('\n');
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
 }
