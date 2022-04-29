d9582039b99f412a3dde9aa1bc6cd2e73bd47d08
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-04-29 12:50:18 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int cpt = 0;
+
+	if (*s != '\0')
+	{
+		cpt++;
+		_strlen_recursion(++s);
+	}
+	return cpt;
+}
