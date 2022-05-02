864435f879b06245415a45e0d03538ef3ae996e1
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-05-02 09:02:23 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ *
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	print("%d", (argc - 1));
+	return (0);
+}
