8748ac89fa7063482dbc70583a91ebbcb23468dc
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-05-04 07:58:08 -0700

    task 4Ã

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *
+ * @str: string given
+ *
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+
