533e99e89e9c8041dfd17f871b2babb0b18fe8d8
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-05-05 01:55:13 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,4 +9,17 @@
  */
 char *_strdup(char *str)
 {
+	int i, size;
+	char *copy;
 
+	for (size = 0; str[size] != '\0; size++)
+		;
+	copy = malloc(size * sizeof(*str) + 1);
+
+	if (str == NULL || copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		copy[i] = str[i];
+	return (copy);
+}
