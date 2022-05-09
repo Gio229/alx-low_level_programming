e4e866196537c4c19ceb84f3acc2a0a3ce600a48
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-05-09 12:19:52 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - definition of a dog
+ *
+ * @name: the name
+ * @age: the age
+ * @owner the owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
