b34043bbe07dcc591f05108d8ac3e03fb691a093
Giovanni ATCHAOUE <aagmaougnon@gmail.com>
2022-05-19 12:47:00 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,24 +3,23 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * 
+ *
  * @h: list
- * 
+ *
  * Return: size_t
  */
 size_t print_list(const list_t *h)
 {
-    size_t number_nodes = 0;
-    const list_t *temp;
+size_t number_nodes = 0;
+const list_t *temp;
 
-    for (temp = h; temp != NULL; temp = temp->next)
-    {
-        if (temp->str != NULL)
-		printf("[%d] %s\n", temp->len, temp->str);
-	else
-		printf("[0] (nil)\n");
-        number_nodes++;
-    }
-
-    return (number_nodes);
+for (temp = h; temp != NULL; temp = temp->next)
+{
+if (temp->str != NULL)
+printf("[%d] %s\n", temp->len, temp->str);
+else
+printf("[0] (nil)\n");
+number_nodes++;
+}
+return (number_nodes);
 }
