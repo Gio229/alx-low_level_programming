a6cea895ac6ed8ab33db5532b8cd43189acf2965
Giovanni <giovanni@irt.bj>
2022-05-19 12:51:29 +0100

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * 
+ * @h: list
+ * 
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+
+}
