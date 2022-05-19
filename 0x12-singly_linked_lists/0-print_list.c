44da43355e90897a54d3263ec8ebb7a762d1a9cb
Giovanni <giovanni@irt.bj>
2022-05-19 13:18:53 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,5 +10,14 @@
  */
 size_t print_list(const list_t *h)
 {
+    size_t number_nodes = 0;
+    list_t *temp;
 
+    for (temp = h; temp != NULL; temp = temp->next)
+    {
+        printf("[%d] %s \n", temp->len, temp->str != NULL ? temp->str : "(nil)");
+        number_nodes++;
+    }
+
+    return (number_nodes);
 }
