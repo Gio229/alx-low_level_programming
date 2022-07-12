ae0480b3daa3b1e7a17b1fa274c3c1f5a6addc77
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-12 13:57:46 +0100

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,9 +5,9 @@
 
 /**
  * string_nconcat - concatenates two strings.
- * @s1
- * @s2
- * @n
+ * @s1: dtring 1
+ * @s2: string 2
+ * @n: n digit
  * Return: a string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -15,25 +15,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i;
 	int len_s1;
 	int len_s2;
-	char * str;
+	char *str;
 
 	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
 	;
 	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
-        ;
+	;
 
 	if (n < len_s2)
 		str = malloc(len_s1 + n * sizeof(char) + 1);
 	else
 		str = malloc(len_s1 + len_s2 + 1);
 	if (str == 0)
-		reurn (NULL);
+		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i+)
+	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	while (s2[i] != '\0')
 		str[i] = s2[i];
 	str[i] = '\0';
 
-	return(str);
+	return (str);
 }
