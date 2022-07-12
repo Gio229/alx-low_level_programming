2acca4f0e7ff4f19da8ca2ec038e75a857f04389
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2022-07-12 14:14:41 +0100

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, len_s1, len_s2;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
 	;
 	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
