70b49b2ef4f5d448272a98c1700db4aae570a5ac
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-03-05 10:34:42 +0100

    task 3 refactoring

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
+ * print_numbers - print numbers from 0 to 9,
  * followed by a new line
+ *
  * Return: void
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
-		if (i == 2 || i == 4)
-			i++;
 		_putchar('0' + i);
-	}
 	_putchar('\n');
 }
