df48df29cb5738d48e9af22d8099e7fbfc1d94dc
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-03-12 18:02:33 +0100

    refactoring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,11 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '.' || str[i] == '\n' || str[i] == '\t' || str[i] == ',' || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i] == ' ' || str[i] == '.' ||
+			str[i] == '\n' || str[i] == '\t' || str[i] == ',' ||
+			str[i] == ';' || str[i] == '!' || str[i] == '?' ||
+			str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
 		{
 			if (str[i + 1] <= 122 && str[i + 1] >= 97)
 				str[i + 1] -= 32;
