d14c2f031b1c531dc1093d5f36122cb5ad4dc32e
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-03-25 13:49:36 +0100

    testing task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,6 +30,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *newer_string;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	if (n < (_strlen(s2)))
 		newer_string = malloc(sizeof(char) * (_strlen(s1) + n + 1));
 	else
