b49facc7cc00d4bdb45c36ab747b7d686534c064
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-03-26 09:51:35 +0100

    setup the last task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments
+ * @argv: arguments vector
+ * Return: 0 - success or error status 98
+ */
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
