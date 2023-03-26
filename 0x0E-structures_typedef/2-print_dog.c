f0e6a694765aca7599ddf6ba25c059f3e902196a
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-03-26 11:19:05 +0100

    refactoring task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name != NULL ? d->name : "(nil)", d->age, (*d).owner != NULL ? d->owner : "(nil)");
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (*d).owner != NULL ? d->owner : "(nil)");
 	}
 }
